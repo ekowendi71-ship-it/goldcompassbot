70dea328c24116d17c6d2de038d4b28ff0c24741
ekowendi71-ship-it <ekowendi71@gmail.com>
2026-07-21 21:26:29 +0700

    Fix TradingView web hook 415

--- bot_server.py
+++ bot_server.py
@@ -1,25 +1,40 @@
 from flask import Flask, request
 import requests
+import os
 
 app = Flask(__name__)
 
-TOKEN = "8465910626:AAFxaiz9IQm1-Uy4s183StUPBdLy7SB2XzM"
-CHAT_ID = "@moneycator"
+TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
+
+@app.route("/")
+def home():
+    return "Moneycator Webhook Online!"
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    data = request.json
+    try:
+        data = request.get_json(silent=True)
 
-    pesan = data.get("message", "Tidak ada pesan")
+        if data and "message" in data:
+            pesan = data["message"]
+        else:
+            pesan = request.data.decode("utf-8")
 
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-    requests.post(url, data={
-        "chat_id": CHAT_ID,
-        "text": pesan
-    })
+        requests.post(
+            url,
+            json={
+                "chat_id": CHAT_ID,
+                "text": pesan
+            }
+        )
 
-    return "OK"
+        return "OK", 200
+
+    except Exception as e:
+        return str(e), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
